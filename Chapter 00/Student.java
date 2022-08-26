3e2da7272c3900917ec95ea07c417b4cf08fbacb
realtonypark <34884758+realtonypark@users.noreply.github.com>
2022-08-26 15:21:29 +0900

    Chapter 00

---
+++ Chapter 00/Student.java
@@ -0,0 +1,11 @@
+public class Student
+{
+    private String myName;
+    private int myAge;
+    private float myHeight;
+
+    public Student()
+    {
+        
+    }
+}
---
+++ Chapter 00/StudentTest.java
@@ -0,0 +1,7 @@
+public class StudentTest 
+{
+    public static void main(String[] args)
+    {
+        //자신의 이름, 나이, 키 출력
+    }
+}
