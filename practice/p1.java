8ba13a706c99f683098df0949d920807cc9b9d7a
realtonypark <34884758+realtonypark@users.noreply.github.com>
2022-08-28 22:20:33 +0900

    practice

--- practice/p1.java
+++ practice/p1.java
@@ -1,3 +1,16 @@
-public class p1 {
-    
+public class p1 
+{
+    public static void main(String[] args)
+    {
+        int a, b;
+        a = 3;
+        b=5;
+
+        double x = 3.0;
+        double y = 5.0;
+
+        System.out.println(a==x);
+        System.out.println(b==y);
+
+    }
 }
---
+++ practice/p2.java
@@ -0,0 +1,11 @@
+public class p2
+{
+    public static void main(String[] args)
+    { 
+        boolean a = false;
+        boolean b = false;
+        boolean c = false;
+
+        System.out.println(!a && !(b && !c));
+    }
+}
