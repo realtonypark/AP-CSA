f2aecee590ad661f1c84ea15d0372264f1a1547d
realtonypark <34884758+realtonypark@users.noreply.github.com>
2022-08-29 02:00:05 +0900

    re

--- Chapter 00/StudentTest.java
+++ Chapter 00/StudentTest.java
@@ -3,7 +3,7 @@ public class StudentTest
     public static void main(String[] args)
     {
         //자신의 이름, 나이, 키 출력
-        Student park = new Student("박승현", 18, 173);
+        Student park = new Student("박승현", 18, 173.2f);
         
         System.out.println("이름: "+park.getmyName());
         System.out.println("나이: "+park.getmyAge());
