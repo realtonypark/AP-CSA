387f054d6978a3f9eb5ad6da3bef95de5ac80324
realtonypark <34884758+realtonypark@users.noreply.github.com>
2022-09-02 00:31:04 +0900

    Chnutil HW

--- Chapter02/chnutil.java
+++ Chapter02/chnutil.java
@@ -1,14 +1,34 @@
 import chn.util.*;
 
-public class chnutil{
+public class Chnutil {
     public static void main(String[] args){
-        System.out.println("Testing Chnutil");    
+        FileInput inFile = new FileInput("input.txt");
+        FileOutput outFile = new FileOutput("output.txt", "append");
+
+        String[] str = scoreCalc(inFile).split("\\s+");
+        int score = Integer.parseInt(str[2])*3/10 + Integer.parseInt(str[3])*3/10 + Integer.parseInt(str[4])*3/10;     
+
+        printScore(outFile, str, score);
+        outFile.close();
+        inFile.close();
+    }
+
+    public static String scoreCalc(FileInput inScores){
+        String scores = "";
+        scores = inScores.readLine();
+        return scores;
+    }
+
+    public static void printScore(FileOutput outScores, String[] str, int score){
+        outScores.println("이름: " + str[0]);
+        outScores.println("나이: " + str[1]);
+        outScores.println("점수: " + score);
     }
 }
 
 /*
  How to run the Chnutil:
- 
+
  javac -cp chnutil.jar chnutil.java
  java -cp chnutil.jar chnutil.java
  */
