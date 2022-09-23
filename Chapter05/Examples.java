dab2754141cdfd02bf3dac8a6ec3f606c4e95431
realtonypark <34884758+realtonypark@users.noreply.github.com>
2022-09-23 13:43:32 +0900

    Added Chapter05

---
+++ Chapter05/Examples.java
@@ -0,0 +1,13 @@
+public class Examples {
+    public static void main(String[] args){
+        String str1 = "test";
+        String str2 = "test";
+
+        String str3 = new String("test");
+
+        System.out.println(str1==str2); // true
+        System.out.println(str1==str3); //false
+        System.out.println(str1.equals(str2)); // true
+        System.out.println(str1.equals(str3)); //false
+    }
+}
