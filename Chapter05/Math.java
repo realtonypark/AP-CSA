139a534db077b560e444652746aedbe92dec066e
realtonypark <34884758+realtonypark@users.noreply.github.com>
2022-10-02 23:52:02 +0900

    Math Class

---
+++ Chapter05/Math.java
@@ -0,0 +1,18 @@
+//import static java.lang.Math;
+
+public class MathEx {
+    public static void main(String[] args){
+        int a = Math.abs(5-10);
+        int b = Math.sqrt(4);
+        int c = Math.pow(2, 10);
+        double d = Math.random();
+        double e = Math.abs(-2.5);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+    
+}
---
+++ Chapter05/MathEx.java
@@ -0,0 +1,20 @@
+import java.lang.Math; //원래 필요 없는데 내 컴퓨터에서는 지금 이게 없으면 코드가 실행이 안됨...
+
+public class MathEx {
+    public static void main(String[] args){
+        int a = Math.abs(-5);
+        double b = Math.sqrt(4);
+        double c = Math.pow(2, 10);
+        double d = Math.random();
+        double e = Math.abs(-2.5);
+        double pi = Math.PI;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(pi);
+    }
+    
+}
