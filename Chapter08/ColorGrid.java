d6451b9f96c2aa996fac1925cc41387fb63089ed
realtonypark <34884758+realtonypark@users.noreply.github.com>
2022-11-20 21:50:50 +0900

    update

--- Chapter08/ColorGrid.java
+++ Chapter08/ColorGrid.java
@@ -8,12 +8,32 @@ public class ColorGrid {
         cols = numCols;
         pixels = new String[numRows][numCols];
 
+        int ind = 0;
+
         for(int i = 0; i < numRows; i++){
-            
+            for(int j = 0; j < numCols; j++){
+                pixels[i][j] = s.substring(ind, ind+1);
+                ind++;
+            }
         }
     }
 
     public void paintRegion(int row, int col, String newColor){
+        
+    }
 
+    public void display(){
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                System.out.print(pixels[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        ColorGrid myGrid = new ColorGrid("abcdefgh", 2, 4);
+        myGrid.display();
     }
 }
